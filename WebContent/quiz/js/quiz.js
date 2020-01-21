e36faef94078d02531f87c316d4132052c5c1fad
r3a1 <r3a1@KCSF1219.KCS-Learning.local>
2020-01-21 14:19:26 +0900

    update quiz

--- WebContent/quiz/js/quiz.js
+++ WebContent/quiz/js/quiz.js
@@ -1,12 +1,12 @@
 $(function(){
 
   //よみこんでなかった 要再実行
-	/*
+/*
   $('.answer').on('change', function(){
 	  console.log("answer selected");
 	  $(this).parent('.answer-label').css('font-size','100px');
   });
-  */
+*/
 
 	$("#final-answer").on('click', function(){
 		console.log("final-answer");
