0aa95ba84ac83504be6503bd876745559bd1ba5b
r3a1 <r3a1@KCSF0916.KCS-Learning.local>
2020-01-22 12:29:11 +0900

    update quiz.js

--- WebContent/quiz/js/quiz.js
+++ WebContent/quiz/js/quiz.js
@@ -1,33 +1,22 @@
-$(function(){
+$(function() {
 
-  //よみこんでなかった 要再実行
+	$('.answer').on('change', function() {
+		$(".answer-content").css({
+			"font-weight" : "initial",
+			"color" : "initial"
+		});
 
-  $('.answer').on('change', function(){
-	  console.log("answer selected");
+		$(this).parent().css({
+			"font-weight" : "bold",
+			"color" : "#05ff05"
+		});
+	});
 
-	  $(".answer-content").css({
-		  "font-weight":"initial",
-		  "color":"initial"
-	  });
-	  console.log("answer selected2");
-
-
-	  $(this).parent().css({
-		  "font-weight":"bold",
-		  "color":"#05ff05"
-	  });
-	  console.log("answer selected3");
-  });
-
-
-
-	$("#final-answer").on('click', function(){
-		console.log($(".answer").prop("checked"));
-		console.log($(".answer:checked").val());
-		if($(".answer:checked").val() != null){
+	$("#final-answer").on('click', function() {
+		if ($(".answer:checked").val() != null) {
 			$(".alert").fadeOut(0);
 			$(".result-content").slideDown(200);
-		}else{
+		} else {
 			$(".alert").slideDown(200);
 		}
 	});
